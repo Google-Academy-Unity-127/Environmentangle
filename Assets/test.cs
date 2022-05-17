84462fbc0d4b02d42bc8011214852b9192fbbc56
Gökhan <gokhan@wellbees.co>
2022-05-18 02:32:05 +0300

    deneme commit

--- Assets/test.cs
+++ Assets/test.cs
@@ -7,7 +7,7 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Buraya birşeyler eklediğimi düşün
     }
 
     // Update is called once per frame
